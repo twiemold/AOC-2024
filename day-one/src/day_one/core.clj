e14c1a8eccf6bbada22c9721614fef05d6058978
Thomas Wiemold <thomas.wiemold@gmail.com>
2024-12-01 15:31:19 -0500

    Solve day one part two.

--- day-one/src/day_one/core.clj
+++ day-one/src/day_one/core.clj
@@ -8,5 +8,13 @@
         data (slurp (if test-mode "test-input.txt" "input.txt"))
         ids (map parse-long (str/split data #"\s\s\s|\n"))
         l-ids (sort (take-nth 2 ids) )
-        r-ids (sort (take-nth 2 (drop 1 ids)) )]
-    (reduce + (map abs (map #(reduce - %) (partition 2 (interleave r-ids l-ids))) ) )))
+        r-ids (sort (take-nth 2 (drop 1 ids)))
+        freq-r-ids (frequencies r-ids)]
+    ;; pt. one
+    ;;(reduce + (map abs (map #(reduce - %) (partition 2 (interleave r-ids l-ids)))))
+    (->> l-ids
+         (map #(get freq-r-ids % 0))
+         (interleave l-ids)
+         (partition 2)
+         (map #(reduce * %))
+         (reduce +))))
